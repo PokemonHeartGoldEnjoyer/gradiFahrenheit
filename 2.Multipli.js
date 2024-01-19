6408c8a739a494330e4fb9a2f7dadfb2adc203c7
PokemonHeartGoldEnjoyer <91133588+PokemonHeartGoldEnjoyer@users.noreply.github.com>
2024-01-19 11:41:13 +0100

    Modifica Multipli

--- 2.Multipli.js
+++ 2.Multipli.js
@@ -1,36 +1,24 @@
-// Definisci una funzione chiamata multipli che pesca un parametro x
-function multipli(x) {
+// Definisci una funzione chiamata prova37 che pesca un parametro x
+function prova37(x) {
     // Controlla se x e divisibile per 3 o 7
     if (x % 3 == 0 || x % 7 == 0) {
       // Se non e 0 viene scritto il risultato 
-      return true;
+      return vero;
     } 
     // Se non e divisibile per 3 o 7 dai come risultato falso  
     else {
-      return false;
+      return falso;
     }
   }
   
-  // Salva il risultato della funzione multipli nel argomento 12 della console
-  console.log(multipli(12));
+  // Salva il risultato della funzione prova37 nel argomento 12 della console
+  console.log(prova37(12));
   
-  // Salva il risultato della funzione multipli nel argomento 14 della console
-  console.log(multipli(14));
+  // Salva il risultato della funzione prova37 nel argomento 14 della console
+  console.log(prova37(14));
   
-  // Salva il risultato della funzione multipli nel argomento 10 della console
-  console.log(multipli(10));
+  // Salva il risultato della funzione prova37 nel argomento 10 della console
+  console.log(prova37(10));
   
-  // Salva il risultato della funzione multipli nel argomento 11 della console
-  console.log(multipli(11)); 
-  
-  /* SPIEGAIONE SOLUZIONE 
-      La funzione multipli prende un parametro x e controlla che sia divisibile per 3 o 7 
-      nel caso sia divisibile il risultato sarà 'true' se al contrario nn fosse divisibile il risultato sarà 'false' 
-  */
-  
-  /* ESEMPIO OUTPUT
-      console.log(multipli(40)); Darà come risultato false non essendo divisibile per 3 o 7 
-      console.log(multipli(35)); Darà come risultato true essendo divisibile per 3 o 7 
-      console.log(multipli(22)); Darà come risultato false non essendo divisibile per 3 o 7 
-      console.log(multipli(21)); Darà come risultato true essendo divisibile per 3 o 7 
-  */
+  // Salva il risultato della funzione prova37 nel argomento 11 della console
+  console.log(prova37(11)); 
